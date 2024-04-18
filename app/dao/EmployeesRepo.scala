26dad9c247d60cf9e8a7373e9954138048632004
unknown <shrutibce@gmail.com>
2024-04-18 10:45:03 +0530

    Refactoring - DI, Packaging and companion object usage

---
+++ app/dao/EmployeesRepo.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import controllers.Employee.toEmployee
+import scalikejdbc._
+import scalikejdbc.config._
+
+class EmployeesRepo {
+  DBs.setupAll()
+  implicit val session = AutoSession
+
+  def getAllEmployees(): List[Employee] = {
+    sql"select * from employees"
+      .map(toEmployee)
+      .list.apply
+  }
+
+//  def getEmployeeById(id: Int): Option[Employee] = {
+//    sql"select * from employees where id = $id"
+//      .map(rs => Employee(rs.int("id"), rs.string("name"), rs.string("designation")))
+//      .single.apply()
+//  }
+//  def insertEmployee(form: EmployeeForm): Unit = {
+//    sql"insert into employees(name, designation) values(${form.name}, ${form.designation})".update.apply()
+//  }
+}
+
